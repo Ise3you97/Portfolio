470d37320010af78e4da3399a190d44bb3e93a99
Ise3you97 <kelvinsolano566@gmail.com>
2024-08-02 20:49:32 -0500

    Responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,77 +1,103 @@
 // src/components/Navigation.js
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navigation.css";
+import { Offcanvas } from "react-bootstrap";
 
 function Navigation() {
-  // Estado para rastrear el enlace activo
   const [activeLink, setActiveLink] = useState("home");
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
-  // Maneja el clic en un enlace para actualizar el enlace activo
+  const handleClose = () => setShowOffcanvas(false);
+  const handleShow = () => setShowOffcanvas(true);
+
   const handleClick = (link) => {
     setActiveLink(link);
+    setShowOffcanvas(false); // Cierra el offcanvas al hacer clic en un enlace
   };
-
-  // Maneja el scroll para actualizar el enlace activo basado en la sección visible
-  const handleScroll = () => {
-    const sections = [
-      "about",
-      "home",
-      "home2",
-      "features",
-      "pricing",
-      "flipcard",
-      "work",
-      "contact",
-    ];
-    let currentSection = "home";
-
-    // Recorre las secciones para determinar cuál está en vista
-    sections.forEach((section) => {
-      const element = document.getElementById(section);
-      if (element) {
-        const { top, bottom } = element.getBoundingClientRect();
-        if (top <= 70 && bottom >= 70) {
-          currentSection = section;
-        }
-      }
-    });
-
-    // Actualiza el enlace activo
-    setActiveLink(currentSection);
-  };
-
-  useEffect(() => {
-    // Agrega el manejador de scroll al montar el componente
-    window.addEventListener("scroll", handleScroll);
-
-    // Limpia el manejador de scroll al desmontar el componente
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg fixed-top">
-      <div className="container-fluid">
-        <div className="">
-          <span className="portafolio-title">PORTFOLIO</span>
+    <>
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            <span className="portafolio-title">PORTFOLIO</span>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={handleShow}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end">
+            <div className="navbar-nav d-none d-lg-flex">
+              <ScrollLink
+                className={`nav-link ${activeLink === "home2" ? "active" : ""}`}
+                to="home2"
+                smooth={true}
+                duration={100}
+                offset={-70}
+                onClick={() => handleClick("home2")}
+              >
+                HOME
+              </ScrollLink>
+              <ScrollLink
+                className={`nav-link ${activeLink === "flipcard" ? "active" : ""}`}
+                to="flipcard"
+                smooth={true}
+                duration={100}
+                offset={-70}
+                onClick={() => handleClick("flipcard")}
+              >
+                ABOUT ME
+              </ScrollLink>
+              <ScrollLink
+                className={`nav-link ${activeLink === "about" ? "active" : ""}`}
+                to="about"
+                smooth={true}
+                duration={100}
+                offset={-70}
+                onClick={() => handleClick("about")}
+              >
+                CAREER
+              </ScrollLink>
+              <ScrollLink
+                className={`nav-link ${activeLink === "work" ? "active" : ""}`}
+                to="work"
+                smooth={true}
+                duration={100}
+                offset={-70}
+                onClick={() => handleClick("work")}
+              >
+                PROJECTS
+              </ScrollLink>
+              <ScrollLink
+                className={`nav-link ${activeLink === "contact" ? "active" : ""}`}
+                to="contact"
+                smooth={true}
+                duration={100}
+                offset={-70}
+                onClick={() => handleClick("contact")}
+              >
+                CONTACT
+              </ScrollLink>
+            </div>
+          </div>
         </div>
-        <button
-          className="navbar-toggler"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNavAltMarkup"
-        >
+      </nav>
+
+      {/* Offcanvas */}
+      <Offcanvas
+        show={showOffcanvas}
+        onHide={handleClose}
+        placement="end"
+        className="offcanvas-custom"
+      >
+        <Offcanvas.Header closeButton>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
           <div className="navbar-nav">
             <ScrollLink
               className={`nav-link ${activeLink === "home2" ? "active" : ""}`}
@@ -124,9 +150,9 @@ function Navigation() {
               CONTACT
             </ScrollLink>
           </div>
-        </div>
-      </div>
-    </nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
